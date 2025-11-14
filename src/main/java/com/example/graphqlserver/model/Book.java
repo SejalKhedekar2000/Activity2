a6bd0682bf12a744db751a3024ec30631839a500
SejalKhedekar2000 <skhedek2@asu.edu>
2025-11-14 11:29:35 -0700

    Changed Book models to JPA entities.

--- src/main/java/com/example/graphqlserver/model/Book.java
+++ src/main/java/com/example/graphqlserver/model/Book.java
@@ -1,15 +1,30 @@
 package com.example.graphqlserver.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
 public class Book {
+
+    @Id
     private String isbn;
+
     private String title;
 
-    private int authorId;
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
 
-    public Book(String isbn, String title, int authorId) {
+    public Book() {}
+
+    public Book(String isbn, String title, Author author) {
         this.isbn = isbn;
         this.title = title;
-        this.authorId = authorId;
+        this.author = author;
     }
 
     public String getIsbn() {
@@ -28,11 +43,11 @@ public class Book {
         this.title = title;
     }
 
-    public int getAuthorId() {
-        return authorId;
+    public Author getAuthor() {
+        return author;
     }
 
-    public void setAuthorId(int authorId) {
-        this.authorId = authorId;
+    public void setAuthor(Author author) {
+        this.author = author;
     }
 }
