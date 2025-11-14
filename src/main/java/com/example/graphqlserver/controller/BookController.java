42994f2e69d84ac8e1b20d5e5ea33c9be7078edd
SejalKhedekar2000 <skhedek2@asu.edu>
2025-11-14 11:50:47 -0700

    Updated BookController.java with JPA repository

--- src/main/java/com/example/graphqlserver/controller/BookController.java
+++ src/main/java/com/example/graphqlserver/controller/BookController.java
@@ -1,18 +1,19 @@
 package com.example.graphqlserver.controller;
 
-import com.example.graphqlserver.dto.input.AddBookInput;
-import com.example.graphqlserver.dto.output.AddBookPayload;
-import com.example.graphqlserver.model.Author;
-import com.example.graphqlserver.model.Book;
-import com.example.graphqlserver.repository.AuthorRepository;
-import com.example.graphqlserver.repository.BookRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
-import java.util.List;
+import com.example.graphqlserver.dto.input.AddBookInput;
+import com.example.graphqlserver.dto.output.AddBookPayload;
+import com.example.graphqlserver.model.Author;
+import com.example.graphqlserver.model.Book;
+import com.example.graphqlserver.repository.AuthorRepository;
+import com.example.graphqlserver.repository.BookRepository;
 
 @Controller
 public class BookController {
@@ -28,23 +29,29 @@ public class BookController {
 
     @QueryMapping
     public List<Book> books() {
-        return bookRepository.getBooks();
+        return bookRepository.findAll();
     }
 
     @QueryMapping
-    public  Book bookByISBN(@Argument("isbn") String isbn) {
-        return bookRepository.getBookByISBN(isbn);
+    public Book bookByISBN(@Argument String isbn) {
+        return bookRepository.findById(isbn).orElse(null);
     }
 
     @MutationMapping
     public AddBookPayload addBook(@Argument AddBookInput input) {
-        Author author = authorRepository.getAuthorById(input.authorId());
+
+        Author author = authorRepository.findById(input.authorId()).orElse(null);
         if (author == null) {
-            throw  new IllegalArgumentException("Author with ID " + input.authorId() + "does not exist");
+            throw new IllegalArgumentException("Author with ID " + input.authorId() + " does not exist");
         }
-        var book = bookRepository.save(input.isbn(), input.title(), input.authorId());
-        author.getBooks().add(book);
-        var out = new AddBookPayload(book);
-        return out;
+
+        Book newBook = new Book();
+        newBook.setIsbn(input.isbn());
+        newBook.setTitle(input.title());
+        newBook.setAuthor(author);
+
+        Book saved = bookRepository.save(newBook);
+
+        return new AddBookPayload(saved);
     }
 }
